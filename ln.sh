79b3267e43738d7e55e13b29900d2c68deac7de5
Mauro <maurolepore@gmail.com>
2020-02-28 20:52:11 -0600

    Add .profile

--- ln.sh
+++ ln.sh
@@ -2,7 +2,7 @@ ln -sv ~/git/.dotfiles/.Rprofile ~
 ln -sv ~/git/.dotfiles/.Renviron ~
 ln -sv ~/git/.dotfiles/.bashrc ~
 ln -sv ~/git/.dotfiles/.gitconfig ~
-# ln -sv ~/git/.dotfiles/.Renviron ~
+ln -sv ~/git/.dotfiles/.profile ~
 # ln -sv ~/git/.dotfiles/.Renviron ~
 # ln -sv ~/git/.dotfiles/.Renviron ~
 # ln -sv ~/git/.dotfiles/.Renviron ~
