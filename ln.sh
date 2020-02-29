c97e5794f4fff2e18c5ad895e2ccecafb18737de
Mauro <maurolepore@gmail.com>
2020-02-28 20:46:48 -0600

    Add .bashrc

--- ln.sh
+++ ln.sh
@@ -1,5 +1,4 @@
 ln -sv ~/git/.dotfiles/.Rprofile ~ 
 ln -sv ~/git/.dotfiles/.Renviron ~
-
-# ln -sv ~/git/.dotfiles/.Renviron ~
+ln -sv ~/git/.dotfiles/.bashrc ~
 # ln -sv ~/git/.dotfiles/.Renviron ~
