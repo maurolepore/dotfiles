d4c33ad8b47c3751226ff59a277d16090797b3fc
Mauro <maurolepore@gmail.com>
2020-03-04 10:44:29 -0600

    Update alias

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,5 @@
 alias mwin="sudo mount /dev/nvme0n1p3 /media/mauro"
 alias cddrop="cd /media/mauro/Users/Mauro/Dropbox\ \(Personal\)"
 alias cddocs="cd /media/mauro/Users/Mauro/Dropbox\ \(Personal\)/PERSONAL/DOCUMENTACION"
+alias dockerrun="~/bin/dockerrun"    
 
