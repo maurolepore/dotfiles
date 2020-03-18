d4e8107541e2976a645c217318be08e0c2831788
Mauro Lepore <maurolepore@gmail.com>
2020-03-18 16:16:17 -0500

    Backup and refresh

--- ln.sh
+++ ln.sh
@@ -1,5 +1,5 @@
 ln -sv ~/git/dotfiles/Rprofile ~/.Rprofile -f
-ln -sv ~/git/dotfiles/bashrc ~/.bashrc -f
+# ln -sv ~/git/dotfiles/bashrc ~/.bashrc -f
 ln -sv ~/git/dotfiles/gitconfig ~/.gitconfig -f
 ln -sv ~/git/dotfiles/profile ~/.profile -f
 ln -sv ~/git/dotfiles/zshrc ~/.zshrc -f
