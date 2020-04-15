10c524b38bdd6124313d8012a4968798c93e7c0f
Mauro Lepore <maurolepore@gmail.com>
2020-04-15 15:12:02 -0500

    New aliases for gh

--- aliases.zsh
+++ aliases.zsh
@@ -3,3 +3,20 @@ alias cddrop="cd /media/mauro/Users/Mauro/Dropbox\ \(Personal\)"
 alias dockerrun="~/bin/dockerrun"    
 alias gbv="git branch -vv"
 alias mwin="sudo mount /dev/nvme0n1p3 /media/mauro"
+
+alias ghic="gh issue create"
+alias ghil="gh issue list"
+alias ghis="gh issue status"
+alias ghiv="gh issue view"
+
+alias ghpc="gh pr create"
+alias ghpco="gh pr checkout"
+alias ghpl="gh pr list"
+alias ghps="gh pr status"
+alias ghpv="gh pr view"
+
+alias ghrcl="gh repo clone"
+alias ghrc="gh repo create"
+alias ghrf="gh repo fork"
+alias ghrv="gh repo view"
+
