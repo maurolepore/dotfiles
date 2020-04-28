cca6e0e4cbcc8f5f6dd3ae95e2fe70df7a3ea47b
Mauro Lepore <maurolepore@gmail.com>
2020-04-28 14:55:16 -0500

    New alias R3 and aliases
    R3 runs R version 3.6.3
    aliases sources aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,8 +1,9 @@
+alias aliases="source ~/.oh-my-zsh/custom/aliases.zsh"
 alias cddocs="cd /media/mauro/Users/Mauro/Dropbox\ \(Personal\)/PERSONAL/DOCUMENTACION"
 alias cddrop="cd /media/mauro/Users/Mauro/Dropbox\ \(Personal\)"
-alias dockerrun="~/bin/dockerrun"    
 alias gbv="git branch -vv"
 alias mwin="sudo mount /dev/nvme0n1p3 /media/mauro"
+alias R3="/usr/bin/R"
 
 alias ghic="gh issue create"
 alias ghil="gh issue list"
