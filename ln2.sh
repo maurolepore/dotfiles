8f94b60fa3188c547d52289522168f54f6218359
Mauro Lepore <maurolepore@gmail.com>
2020-11-18 14:23:39 -0600

    Remove ln2.sh

--- ln2.sh
+++
@@ -1,8 +0,0 @@
-ln -sv $PWD/Rprofile ~/.Rprofile -f
-# ln -sv $PWD/bashrc ~/.bashrc -f
-ln -sv $PWD/gitconfig ~/.gitconfig -f
-ln -sv $PWD/profile ~/.profile -f
-ln -sv $PWD/zshrc ~/.zshrc -f
-ln -sv $PWD/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh -f
-ln -sv $PWD/rstudio-prefs.json ~/.config/rstudio/rstudio-prefs.json -f
-
