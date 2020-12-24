d3fb2cd58f371a349670a2188d0ec3808adba8d8
Mauro Lepore <maurolepore@gmail.com>
2020-12-24 15:00:59 -0300

    Add keybindings

--- ln.sh
+++ ln.sh
@@ -8,4 +8,6 @@ ln -sv ~/git/dotfiles/rstudio-prefs.json ~/.config/rstudio/rstudio-prefs.json -f
 ln -sv ~/git/dotfiles/r.snippets  ~/.config/rstudio/snippets/r.snippets -f
 ln -sv ~/git/dotfiles/markdown.snippets  ~/.config/rstudio/snippets/markdown.snippets -f
 ln -sv ~/git/dotfiles/addins.json  ~/.config/rstudio/keybindings/addins.json -f
+ln -sv ~/git/dotfiles/editor_bindings.json  ~/.config/rstudio/keybindings/editor_bindings.json -f
+ln -sv ~/git/dotfiles/rstudio_bindings.json  ~/.config/rstudio/keybindings/rstudio_bindings.json -f
 
