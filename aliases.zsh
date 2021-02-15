ad6a94b2b947f51b831661878d683ce302eba6ec
Mauro Lepore <maurolepore@gmail.com>
2021-02-15 07:16:01 -0600

    Alias R4

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,9 @@ alias cddrop="cd /media/mauro/Users/Mauro/Dropbox\ \(Personal\)"
 alias gbv="git branch -vv"
 alias mwin="sudo mount /dev/nvme0n1p3 /media/mauro"
 alias R3="/usr/bin/R"
+alias Rscript="/opt/R/4.0.0/bin/Rscript"
+alias R="/opt/R/4.0.0/bin/R --no-save"
+# alias R="/usr/bin/R --no-save"
 
 alias ghic="gh issue create"
 alias ghil="gh issue list"
@@ -49,5 +52,4 @@ alias rapp="Rscript -e 'shiny::runApp()'"
 
 alias l="ls -1"
 
-alias R="/usr/bin/R --no-save"
 
