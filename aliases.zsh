d27f01b7ee35cd4dee06f9f2e127ca130a05d7bc
Mauro Lepore <maurolepore@gmail.com>
2021-12-02 10:21:15 -0600

    New alias to az

--- aliases.zsh
+++ aliases.zsh
@@ -87,4 +87,4 @@ alias R="R --no-save --no-restore-data"
 alias rbranchrename="Rscript -e 'usethis::git_default_branch_rename()'"
 alias rbranchrediscover="Rscript -e 'usethis::git_default_branch_rediscover()'"
 
-
+alias az2='az boards work-item update --organization "https://dev.azure.com/2DegreesInvesting"'
