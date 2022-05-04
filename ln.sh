d5692f7222208aedea2dad745daf21ee79d34c16
Mauro Lepore <maurolepore@gmail.com>
2022-05-04 15:24:15 -0600

    Tidy ln.sh

--- ln.sh
+++ ln.sh
@@ -1,14 +1,14 @@
-ln -sv ~/git/dotfiles/addins.json  ~/.config/rstudio/keybindings/addins.json -f
-ln -sv ~/git/dotfiles/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh -f
-ln -sv ~/git/dotfiles/editor_bindings.json  ~/.config/rstudio/keybindings/editor_bindings.json -f
-ln -sv ~/git/dotfiles/gitconfig ~/.gitconfig -f
-ln -sv ~/git/dotfiles/gitignore ~/.gitignore -f
-ln -sv ~/git/dotfiles/markdown.snippets  ~/.config/rstudio/snippets/markdown.snippets -f
-ln -sv ~/git/dotfiles/pam_environment ~/.pam_environment -f
-ln -sv ~/git/dotfiles/profile ~/.profile -f
-ln -sv ~/git/dotfiles/Rprofile ~/.Rprofile -f
-ln -sv ~/git/dotfiles/r.snippets  ~/.config/rstudio/snippets/r.snippets -f
-ln -sv ~/git/dotfiles/rstudio_bindings.json  ~/.config/rstudio/keybindings/rstudio_bindings.json -f
-ln -sv ~/git/dotfiles/rstudio-prefs.json ~/.config/rstudio/rstudio-prefs.json -f
-ln -sv ~/git/dotfiles/vimrc ~/.vimrc -f
-ln -sv ~/git/dotfiles/zshrc ~/.zshrc -f
+ln -svf ~/git/dotfiles/addins.json  ~/.config/rstudio/keybindings/addins.json
+ln -svf ~/git/dotfiles/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh 
+ln -svf ~/git/dotfiles/editor_bindings.json  ~/.config/rstudio/keybindings/editor_bindings.json 
+ln -svf ~/git/dotfiles/gitconfig ~/.gitconfig 
+ln -svf ~/git/dotfiles/gitignore ~/.gitignore
+ln -svf ~/git/dotfiles/markdown.snippets  ~/.config/rstudio/snippets/markdown.snippets
+ln -svf ~/git/dotfiles/pam_environment ~/.pam_environment 
+ln -svf ~/git/dotfiles/profile ~/.profile 
+ln -svf ~/git/dotfiles/Rprofile ~/.Rprofile
+ln -svf ~/git/dotfiles/r.snippets  ~/.config/rstudio/snippets/r.snippets 
+ln -svf ~/git/dotfiles/rstudio_bindings.json  ~/.config/rstudio/keybindings/rstudio_bindings.json 
+ln -svf ~/git/dotfiles/rstudio-prefs.json ~/.config/rstudio/rstudio-prefs.json 
+ln -svf ~/git/dotfiles/vimrc ~/.vimrc 
+ln -svf ~/git/dotfiles/zshrc ~/.zshrc
