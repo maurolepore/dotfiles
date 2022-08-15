238a7ab4d435fc0f74233f9b9ca9643dbf86579e
Mauro Lepore <maurolepore@gmail.com>
2022-08-15 21:06:53 +0200

    Prune needless dotfils

--- ln.sh
+++ ln.sh
@@ -1,7 +1,6 @@
 ln -svf ~/git/dotfiles/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh 
 ln -svf ~/git/dotfiles/gitconfig ~/.gitconfig 
 ln -svf ~/git/dotfiles/gitignore ~/.gitignore
-ln -svf ~/git/dotfiles/pam_environment ~/.pam_environment 
 ln -svf ~/git/dotfiles/profile ~/.profile 
 ln -svf ~/git/dotfiles/Rprofile ~/.Rprofile
 ln -svf ~/git/dotfiles/vimrc ~/.vimrc 
